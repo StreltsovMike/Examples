a53cbe846877ba6e3248f17bd1a50bb3f6b918d8
Streltsov_Mike <streltsovmike@yandex.ru>
2022-06-22 19:59:51 +0300

    Add Example004_Mass

---
+++ Example004_Mass/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array1 = new int[] { 3,3,4,2,5,7,2,8,13,4,9 };
+var str = string.Join(" ", array1);
+Console.Write("Массив :");
+Console.WriteLine(str);
+
+int count = 0;
+int max = array1[1];
+
+while(count<11) 
+{
+    if(array1[count] > max) 
+    {
+        max = array1[count];
+    }
+    count++;
+}
+
+Console.Write("Максимальное число : " + max);
