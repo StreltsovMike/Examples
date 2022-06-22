a5b8483b206ce2affc35f973672c0b30f915d640
Streltsov_Mike <streltsovmike@yandex.ru>
2022-06-22 18:37:29 +0300

    Add Example002_Sum

---
+++ Example002_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int A = new Random(). Next(1, 100);
+Console.WriteLine("Первое число: " + A);
+int B = new Random(). Next(1, 100);
+Console.WriteLine("Второе число: " + B);
+int C = A + B;
+Console.WriteLine ("Сумма: " + C);
